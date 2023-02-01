1bfbac6e374532dc708368bbff93e0630ca68a71
ihagedo <isaachagedorn@gmail.com>
2023-02-01 18:52:30 -0500

    added basic octo functionality

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -5,23 +5,29 @@ use log::{info, warn};
 use std::io::{self, Write};
 use anyhow::{Context, Result};
 
+use crate::octo::initOcto;
+
+mod octo;
+
 
 #[derive(Parser)]
 struct Cli {
-    pattern: String,
     path: std::path::PathBuf,
 }
 
 #[derive(Debug)]
 struct CustomError(String);
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     env_logger::init();
     let args = Cli::parse();
     let stdout = io::stdout();
     let mut handle_lock = stdout.lock();
-
-
+    let token: String = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required").into();
+    let repo = octo::getRepo(token.clone(), "rust-lang".into(), "rust".into()).await;
+    let page = octo::getIssue(token.clone(), "rust-lang".into(), "rust".into()).await;
+    
     // TODO optimize with BefReader    
     let content = std::fs::read_to_string(&args.path).unwrap();
     let path = &args.path;
@@ -29,6 +35,11 @@ fn main() -> Result<()> {
         .with_context(|| format!("could not read file `{}`", path.display()))?;
 
     writeln!(handle_lock, "file content: {}", content);
+    writeln!(handle_lock, "{:#?}", repo.license);
+    for issue in page
+    {
+        writeln!(handle_lock, "{}", issue.title);
+    }
     Ok(())
 }
 

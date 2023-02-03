10e9c54235d937d0003fa6a47a7ba7bfda9ebfbc
ihagedo <isaachagedorn@gmail.com>
2023-02-03 05:24:56 -0500

    added octocrab src for mod

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -1,11 +1,12 @@
 #![allow(unused)]
 
 use clap::Parser;
-use log::{info, warn};
+use log::{info, warn, debug};
 use std::io::{self, Write};
 use anyhow::{Context, Result};
 
-use crate::octo::initOcto;
+use dotenv::dotenv;
+use std::env;
 
 mod octo;
 
@@ -21,12 +22,14 @@ struct CustomError(String);
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
+    dotenv().ok();
     let args = Cli::parse();
     let stdout = io::stdout();
     let mut handle_lock = stdout.lock();
-    let token: String = std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required").into();
+    let token: String = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required").into();
     let repo = octo::getRepo(token.clone(), "rust-lang".into(), "rust".into()).await;
-    let page = octo::getIssue(token.clone(), "rust-lang".into(), "rust".into()).await;
+    info!("Retrieved {}", repo.name);
+    let page = octo::getIssues(token.clone(), "rust-lang".into(), "rust".into()).await;
     
     // TODO optimize with BefReader    
     let content = std::fs::read_to_string(&args.path).unwrap();
@@ -38,7 +41,7 @@ async fn main() -> Result<()> {
     writeln!(handle_lock, "{:#?}", repo.license);
     for issue in page
     {
-        writeln!(handle_lock, "{}", issue.title);
+        writeln!(handle_lock, "{:#?}", issue.closed_at);
     }
     Ok(())
 }

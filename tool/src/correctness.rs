7134664b26f08d78bf4cd815991616330ce7aac0
Colleen Granelli <colleengranelli@pal-nat186-184-117.itap.purdue.edu>
2023-02-12 17:53:00 -0500

    changing correctness

--- tool/src/correctness.rs
+++ tool/src/correctness.rs
@@ -1,56 +1,5 @@
 
-use octocrab::{tokio::net::windows::named_pipe::PipeEnd::Client;
-, Error};
-
-// all issues closed (1/3 weight)
-async fn are_all_issues_closed(repo: &str, client: &Client) -> Result<i32, Error> {
-    let issues_url = format!("/repos/{}/issues", repo);
-    let issues_response = client.get(issues_url).send().await?;
-
-    let issues = issues_response.json::<Vec<serde_json::Value>>().await?;
-
-    for issue in issues {
-        let state = issue["state"].as_str().unwrap();
-        if state != "closed" {
-            return Ok(0);
-        }
-    }
-
-    Ok(1)
-}
-
-// has tests (1/3 weight)
-async fn has_testing_suite(repo: &str, client: &Client) -> Result<i32, Error> {
-    let contents_url = format!("/repos/{}/contents", repo);
-    let contents_response = client.get(contents_url).send().await?;
-
-    let contents = contents_response.json::<Vec<serde_json::Value>>().await?;
-
-    for content in contents {
-        let content_name = content["name"].as_str().unwrap();
-        if content_name == "tests" {
-            return Ok(1);
-        }
-    }
-
-    Ok(0)
-}
-
-// check number of releases (1/3 weight)
-async fn check_number_of_releases(repo: &str, client: &Client) -> Result<i32, Error> {
-    let releases_url = format!("/repos/{}/releases", repo);
-    let releases_response = client.get(releases_url).send().await?;
-
-    let releases = releases_response.json::<Vec<serde_json::Value>>().await?;
-
-    if releases.len() > 10 {
-        Ok(1)
-    } else {
-        Ok(0)
-    }
-}
-
-async fn get_weighted_score(repo: &str, client: &Client) -> Result<f64, Error> {
+pub async fn get_weighted_score(repo: &str, client: &Client) -> Result<f64, Error> {
     let first = are_all_issues_closed(repo, client).await?;
     let second = has_testing_suite(repo, client).await?;
     let third = check_number_of_releases(repo, client).await?;
--- tool/src/octo.rs
+++ tool/src/octo.rs
@@ -3,6 +3,7 @@ extern crate octocrab;
 use serde_json;
 
 use octocrab::{Octocrab, Page, Result, models::{self, repos::RepoCommit}, params};
+use octocrab::{tokio::net::windows::named_pipe::PipeEnd::Client, Error};
 use std::error::Error;
 use chrono::{Duration, Utc, NaiveDate};
 
@@ -254,4 +255,50 @@ pub async fn get_contributors_with_percentages(token: String, owner: String, rep
     }
 
     Ok(result)
+}
+
+// Our group determined that the presense of a README was the most important part of
+// the ramp up score. A README allows others to get versed in a project and learn what
+// its about. If there is a README the repository will recieve a score of 1. If not
+// it will get a score of 0.
+pub async fn has_readme(repo: &str, client: &Client) -> Result<i32, Error> {
+    let file_list_url = format!("/repos/{}/git/trees/master?recursive=1", repo);
+    let file_list_response = client
+        .get(file_list_url)
+        .send()
+        .await?;
+
+    let file_list_content = file_list_response.json::<serde_json::Value>().await?;
+    let files = file_list_content["tree"].as_array().unwrap();
+
+    for file in files {
+        let path = file["path"].as_str().unwrap();
+        if path == "README.md" {
+            return Ok(1);
+        }
+    }
+
+    Ok(0)
+}
+
+pub async fn check_multiple_readmes(repo: &str, client: &Client) -> Result<i32, Error> {
+    let contents_url = format!("/repos/{}/contents/", repo);
+    let contents_response = client.get(contents_url).send().await?;
+
+    let contents = contents_response.json::<Vec<serde_json::Value>>().await?;
+
+    let mut readme_count = 0;
+
+    for content in contents {
+        let name = content["name"].as_str().unwrap();
+        if name.to_lowercase().starts_with("readme") {
+            readme_count += 1;
+        }
+    }
+
+    if readme_count > 1 {
+        Ok(1)
+    } else {
+        Ok(0)
+    }
 }

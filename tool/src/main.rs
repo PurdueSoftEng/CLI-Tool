5b8896babb283903a0e2a35dd47eb358d40b9dda
ihagedo <isaachagedorn@gmail.com>
2023-02-12 23:49:41 -0500

    added loop structure and sort

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -3,6 +3,7 @@
 use clap::Parser;
 use log::{info, warn, debug, LevelFilter};
 use std::io::{self, Write};
+use std::ops::IndexMut;
 use anyhow::{Context, Result};
 
 use dotenv::dotenv;
@@ -95,6 +96,22 @@ fn extract_owner_and_repo(url: &str) -> Option<(String, String)> {
     Some((captures[1].to_string(), captures[2].to_string()))
 }
 
+fn sort_repositories(repositories: &mut Vec<GithubRepo>) {
+    repositories.sort_by(|a, b| {
+        let overall_cmp = b.overall().cmp(&a.overall());
+        if overall_cmp == Ordering::Equal {
+            let bus_cmp = b.bus().cmp(&a.bus());
+            if bus_cmp == Ordering::Equal {
+                a.license().cmp(&b.license())
+            } else {
+                bus_cmp
+            }
+        } else {
+            overall_cmp
+        }
+    });
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
@@ -106,37 +123,30 @@ async fn main() -> Result<()> {
 
     // let owner = "MinecraftForge";
     // let repo_name = "ForgeGradle";
-    let repos_list = read_github_repos_from_file(&args.path);
-    let repo_info = extract_owner_and_repo(repos_list.first().unwrap().url.as_str());
-    let owner = repo_info.clone().unwrap().0;
-    let repo_name = repo_info.clone().unwrap().1;
-    let repo = octo::get_repo(token.clone(), owner.clone(), repo_name.clone()).await;
+    let mut repos_list = read_github_repos_from_file(&args.path);
+    for repository in repos_list.as_mut_slice()
+    {
+        let repo_info = extract_owner_and_repo(&repository.url);
+        let owner = repo_info.clone().unwrap().0;
+        let repo_name = repo_info.clone().unwrap().1;
+        let repo = octo::get_repo(token.clone(), owner.clone(), repo_name.clone()).await;
+
+        let mut resp = octo::get_license(token.clone(), owner.clone().as_str(), repo_name.clone().as_str()).await;
+        let data_layer = resp.get_mut("data").expect("Data key not found");
+        let repository_layer = data_layer.get_mut("repository").expect("Repository key not found");
+        let license_layer = repository_layer.get_mut("licenseInfo").expect("License key not found");
+        if license_layer.get("key").is_some()
+        {
+            let license_score = calc_license::calc_licenses(license_layer.get("key").unwrap().to_string()).await;
+        }
+        else
+        {
+            let license_score = 0;
+        }
+    }
+
+    sort_repositories(repos_list.as_mut());
     //info!("Retrieved {}", repo.name);
-
-    let t = calc_responsive_maintainer::calc_commit_bin_size(0.1, repo.clone());
-    let binned_issues = octo::get_issues(token.clone(), owner.clone(), repo_name.clone(), t as i64).await.unwrap();
-    let average_duration: f64 = calc_responsive_maintainer::get_avg_issue_duration(binned_issues);
-    let commit_pages = octo::get_all_commits(token.clone(), owner.clone(), repo_name.clone()).await.unwrap();
-    //let temp = calc_responsive_maintainer::calc_duration_between_first_and_last_commit(commit_pages.clone());
-    //let t = octo::get_duration_between_first_and_last_commit(token.clone(), owner.into(), repo_name.into()).await.unwrap();
-    //let t = 0.0;
-    let responsive_maintainer_summation: f64 = calc_responsive_maintainer::calc_responsive_maintainer_summation(commit_pages, t);
-    let uses_workflows = octo::uses_workflows(token.clone(), owner.clone(), repo_name.clone()).await.unwrap();
-
-    let responsive_scroe = calc_responsive_maintainer::calc_responsive_maintainer(1.0, uses_workflows, responsive_maintainer_summation, average_duration);
-
-    let mut resp = octo::get_license(token.clone(), owner.clone().as_str(), repo_name.clone().as_str()).await;
-    let data_layer = resp.get_mut("data").expect("Data key not found");
-    let repository_layer = data_layer.get_mut("repository").expect("Repository key not found");
-    let license_layer = repository_layer.get_mut("licenseInfo").expect("License key not found");
-    if license_layer.get("key").is_some()
-    {
-        let license_score = calc_license::calc_licenses(license_layer.get("key").unwrap().to_string()).await;
-    }
-    else
-    {
-        let license_score = 0;
-    }
 
     Ok(())
 }

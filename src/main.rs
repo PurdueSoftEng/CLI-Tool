f0c66273f99a48e5ef7d293d38f6156ca8676e3e
Albert Lee <lee3445@purdue.edu>
2023-02-25 13:22:20 -0500

    remove debug stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 #[tokio::main]
 async fn main() {
-    println!("{:?}", cli::working());
-    let res = cli::rate("https://github.com/ljharb/qs", env!("GITHUB_TOKEN"))
-        .await
-        .unwrap();
-    println!("{:?}", res);
-    println!("{:?}", res.responsive());
-    println!("Hello, world!");
+    println!(":)");
 }

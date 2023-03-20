306c4b1f33e9cdeda9fe1d7f47a0542c86127489
Isaac Hagedorn <ihagedo@purdue.edu>
2023-03-20 14:28:03 -0400

    fixed scripts for logging

--- scripts/rank.sh
+++ scripts/rank.sh
@@ -4,4 +4,4 @@ echo "$GITHUB_TOKEN"
 if [ -e "log/run.log" ]; then
     rm log/run.log
 fi
-cargo run -- $1 > log/run.log 2>&1
+cargo run -- $1
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 use clap::Parser;
 use log::{info, warn, debug, LevelFilter};
+use log4rs::append::file::FileAppender;
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::config::{Appender, Config, Root};
 use std::io::{self, Write};
 use anyhow::{Context, Result};
 extern crate octocrab;
@@ -12,6 +15,7 @@ use serde_json::{json, Value, Map};
 
 use dotenv::dotenv;
 use std::env;
+use std::path::Path;
 
 use regex::Regex;
 
@@ -45,7 +49,12 @@ struct Output {
 
 }*/
 
-#[derive(Debug)]
+// #[derive(Debug)]
+// struct GithubRepo {
+//     url: String,
+//     scores: Vec<f32>,
+// }
+#[derive(Debug, Clone)]
 struct GithubRepo {
     url: String,
     scores: Vec<f32>,
@@ -139,44 +148,59 @@ fn extract_owner_and_repo(url: &str) -> Option<(String, String)> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::init();
+    //env_logger::init();
     dotenv().ok();
     let args = Cli::parse();
     let stdout = io::stdout();
     let mut handle_lock = stdout.lock();
     let token: String = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN env variable is required").into();
+    let log_file = env::var("LOG_FILE").expect("LOG_FILE environment variable not set");
+    let log_level = env::var("LOG_LEVEL").expect("LOG_LEVEL environment variable not set");
+    let log_path = Path::new(&log_file);
+
+    let mut logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build(log_path)?;
+
+    let log_level = match log_level.as_str() {
+        "1" => LevelFilter::Error,
+        "2" => LevelFilter::Warn,
+        "3" => LevelFilter::Info,
+        _ => panic!("Invalid log level specified: {}", log_level),
+    };
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder()
+                   .appender("logfile")
+                   .build(log_level))?;
+
+    log4rs::init_config(config)?;
+
+    info!("Logger initialized");
+
     let mut repos_list = read_github_repos_from_file(&args.path);
+    //println!("{}", repos_list);
     let repo_info = extract_owner_and_repo(repos_list.first().unwrap().url.as_str());
     let owner = repo_info.clone().unwrap().0;
     let repo_name = repo_info.clone().unwrap().1;
 
     let repo = octo::get_repo(token.clone(), owner.clone(), repo_name.clone()).await;
 
-    for mut repository in repos_list{
-        let scores_list = calc_metrics(token.clone(), owner.clone(), repo_name.clone()).await;
-        for score in scores_list{
-            repository.responsive_set(score);
-            repository.license_set(score);
-            repository.rampup_set(score);
-            repository.correct_set(score);
-            repository.bus_set(score);
-            repository.overall_set(score);
-        }
-        
-        //create_ndjson(repository.url.as_str(), repository.overall(), repository.rampup(), repository.correct(), repository.bus(), repository.responsive(), repository.license());
+    for repository in &mut repos_list{
+        calc_metrics(repository, token.clone(), owner.clone(), repo_name.clone()).await;
+        //sort_repositories(repos_list.as_mut());
         create_ndjson(repository.url.as_str(), repository.overall(), repository.rampup(), repository.correct(), repository.bus(), repository.responsive(), repository.license());
-
     }
 
     Ok(())
     
 }
 
-async fn calc_metrics(token: String, owner: String, repo: String) -> Vec<f32> {
-    let mut scores_vec = Vec::new();
+async fn calc_metrics(repository: &mut GithubRepo, token: String, owner: String, repo: String) {
     let mut issue_response_times = octo::get_issue_response_times(token.clone(), owner.clone(), repo.clone()).await.unwrap();
     let mut responsive_score = calc_responsive_maintainer::calc_responsive_maintainer(issue_response_times[0], issue_response_times[1]) as f32;
-    scores_vec.push(responsive_score);
+    repository.responsive_set(responsive_score);
 
     let mut resp = octo::get_license(token.clone(), owner.clone().as_str(), repo.clone().as_str()).await;
     let data_layer = resp.get_mut("data").expect("Data key not found");
@@ -186,39 +210,38 @@ async fn calc_metrics(token: String, owner: String, repo: String) -> Vec<f32> {
     if license_layer.get("key").is_some()
     {
         license_score = calc_license::calc_licenses(license_layer.get("key").unwrap().to_string()).await;
+        println!("license score: {}", license_score);
     }
-    scores_vec.push(license_score as f32);
+    repository.license_set(license_score.into());
 
     let octo = Octocrab::builder().personal_token(token.clone()).build().unwrap();
 
-    let ramp_up_score = ramp_up::get_weighted_score(octo.clone(), owner.clone(), repo.clone()).await.unwrap();
-    //let ramp_up_score = 0;
-    scores_vec.push(ramp_up_score as f32);
+    let mut ramp_up_score = ramp_up::get_weighted_score(octo.clone(), owner.clone(), repo.clone()).await.unwrap();
+    //println!("ramp up score: {}", ramp_up_score);
+    repository.rampup_set(ramp_up_score as f32);
 
-    //let correctness_score = correctness::get_weighted_score(token.clone(), owner.clone(), repo.clone()).await.unwrap();
-    let correctness_score = 0;
-    scores_vec.push(correctness_score as f32);
+    let mut correctness_score = correctness::get_weighted_score(token.clone(), owner.clone(), repo.clone()).await.unwrap();
+    //println!("correctness: {}", correctness_score);
+    //let correctness_score = 0;
+    repository.correct_set(correctness_score as f32);
 
-    let bus_factor_score = 0;
+    let mut bus_factor_score = 0;
     //let bus_factor_score = calc_bus_factor::calculate_bus_factor(token.clone(), owner.clone(), repo.clone()).await;
-    scores_vec.push(bus_factor_score as f32);
+    repository.bus_set(bus_factor_score as f32);
 
-    let net_score_score = 0.0;
-    scores_vec.push(net_score_score as f32);
-
-    scores_vec   
+    let mut net_score_score = 0.0;
+    repository.overall_set(net_score_score);
 }
-
 
 fn create_ndjson(url: &str, net_score: f32, ramp_up_score: f32, correctness_score: f32, bus_factor_score: f32, responsive_maintainer_score: f32, license_score: f32) {
     let json = json!({
         "URL": url,
-        "NET_SCORE": net_score,
-        "RAMP_UP_SCORE": ramp_up_score,
-        "CORRECTNESS_SCORE": correctness_score,
-        "BUS_FACTOR_SCORE": bus_factor_score,
-        "RESPONSIVE_MAINTAINER_SCORE": responsive_maintainer_score,
-        "LICENSE_SCORE": license_score
+        "NET_SCORE": format!("{:.1}", net_score),
+        "RAMP_UP_SCORE": format!("{:.1}", ramp_up_score),
+        "CORRECTNESS_SCORE": format!("{:.1}", correctness_score),
+        "BUS_FACTOR_SCORE": format!("{:.1}", bus_factor_score),
+        "RESPONSIVE_MAINTAINER_SCORE": format!("{:.1}", responsive_maintainer_score),
+        "LICENSE_SCORE": format!("{:.1}", license_score),
     });
     let ndjson = json.to_string();
     println!("{}", ndjson);
@@ -257,3 +280,19 @@ mod tests {
         assert_eq!(repos.get(2).unwrap().url, String::from("https://www.npmjs.com/package/browserify"));
     }
 }
+
+// fn sort_repositories(repositories: &mut Vec<GithubRepo>) {
+//     repositories.sort_by(|a, b| {
+//         let overall_cmp = b.overall().cmp(&a.overall());
+//         if overall_cmp == Ordering::Equal {
+//             let bus_cmp = b.bus().cmp(&a.bus());
+//             if bus_cmp == Ordering::Equal {
+//                 a.license().cmp(&b.license())
+//             } else {
+//                 bus_cmp
+//             }
+//         } else {
+//             overall_cmp
+//         }
+//     });
+// }
